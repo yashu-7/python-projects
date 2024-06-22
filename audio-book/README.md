53b8c6c0c793e96eaeed6899d41917fc2731d404
yashu-7 <yaswanthreddypalle@gmail.com>
2024-06-22 18:06:38 +0530

    Added a read aloud button

--- audio-book/README.md
+++ audio-book/README.md
@@ -6,7 +6,7 @@
 * ~~Adding gui to upload pdf~~
 * changes in GUI
     1. Display a box to show what file is uploaded
-    2. Add dedicated speak button
+    2. Add dedicated speak button (for male and female)
 * Read from images option
 * Edit and save changes to PDF
 * Pause and play option
--- audio-book/main.py
+++ audio-book/main.py
@@ -18,25 +18,32 @@ class PDFreader():
         self.text = ctk.CTkTextbox(master=self.display, height=260, width=620)
         self.text.pack(fill='both', expand=True)
 
+        self.speak_btn = ctk.CTkButton(self.app, width=78, height=25, text='Read aloud', command=self.start_speaking)
+        self.speak_btn.place(x=281, y=300)
+
         self.upload_button = ctk.CTkButton(self.app, width=78, height=25, text="Browse File", command=self.upload)
         self.upload_button.place(x=281, y=270)
+        
+        self.extracted_text = ""  # Variable to store extracted text
+        
         self.app.mainloop()
 
     def upload(self):
         file_path = tk.filedialog.askopenfile(title='Select PDF', filetypes=[("PDF files", "*.pdf")])
         if file_path:
             # print(file_path)
-            # data = self.extract_text(file_path.name)
-            # self.display_frame(data)
+            # Starting a new thread for processing the file
             threading.Thread(target=self.process, args=(file_path.name,)).start()
 
     def process(self, file_path):
-        data = self.extract_text(file_path)
-        self.display_frame(data)
-        self.start_speaking(data)
+        self.extracted_text = self.extract_text(file_path)
+        self.display_frame(self.extracted_text)
+        # self.start_speaking(data)
 
-    def start_speaking(self, text):
-        threading.Thread(target=self.say, args=(text,)).start()
+    def start_speaking(self):
+        if self.extracted_text:
+            # Starting a new thread for text-to-speech
+            threading.Thread(target=self.say, args=(self.extracted_text,)).start()
 
     # This is used to start the engine and read the string it is passed 
     def say(self, command):
